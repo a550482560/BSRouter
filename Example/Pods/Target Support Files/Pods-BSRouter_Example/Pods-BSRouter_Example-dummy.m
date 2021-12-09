c4e83d6b2c51da070985c16d197c903415e0f2b8
a550482560 <yuanyuan53@leju.com>
2021-12-09 12:38:00 +0800

    描述

---
+++ Example/Pods/Target Support Files/Pods-BSRouter_Example/Pods-BSRouter_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BSRouter_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BSRouter_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-BSRouter_Tests/Pods-BSRouter_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BSRouter_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BSRouter_Tests
+@end
