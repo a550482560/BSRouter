329634b33984a7e2edcd31192394c9e4faf5dd22
a550482560 <yuanyuan53@leju.com>
2021-12-09 12:34:03 +0800

    Initial commit

---
+++ Example/BSRouter/BSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSAppDelegate.h
+//  BSRouter
+//
+//  Created by 550482560@qq.com on 12/09/2021.
+//  Copyright (c) 2021 550482560@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/BSRouter/BSViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSViewController.h
+//  BSRouter
+//
+//  Created by 550482560@qq.com on 12/09/2021.
+//  Copyright (c) 2021 550482560@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BSViewController : UIViewController
+
+@end
